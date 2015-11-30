926e82a809cd7c74bbf719f9dd3812fd229c2af8
Hironori Ichimiya <hiron@hironytic.com>
2015-12-01 08:50:38 +0900

    Brush up

--- src/main.jsx
+++ src/main.jsx
@@ -8,22 +8,20 @@ import * as ActivityActions from "./actions/ActivityActions";
 
 import App from "./components/App.jsx";
 
-ReactDOM.render(<App/>, document.getElementById("app-content"));
-
-let url = window.location.href;
-let fragmentIx = url.indexOf("#");
-if (fragmentIx >= 0) {
-  let fragment = url.substring(fragmentIx + 1);
-  ActivityActions.activityChanged(fragment);
-}
-
-Rx.Observable.fromEvent(window, "hashchange")
-.subscribe(() => {
+function notifyActivityChanged() {
   let fragment = null;
   let url = window.location.href;
   let fragmentIx = url.indexOf("#");
   if (fragmentIx >= 0) {
     fragment = url.substring(fragmentIx + 1);
   }
-  ActivityActions.activityChanged(fragment);
+  ActivityActions.activityChanged(fragment);  
+}
+
+ReactDOM.render(<App/>, document.getElementById("app-content"));
+
+notifyActivityChanged();
+Rx.Observable.fromEvent(window, "hashchange")
+.subscribe(() => {
+  notifyActivityChanged();
 });
