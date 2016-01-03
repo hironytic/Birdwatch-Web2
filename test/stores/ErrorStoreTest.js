a49ea99addf581df3e5e0310146a8bef9bd21f3e
Hironori Ichimiya <hiron@hironytic.com>
2016-01-03 20:56:12 +0900

    Add test cases for ErrorStore

--- test/stores/ErrorStoreTest.js
+++ test/stores/ErrorStoreTest.js
@@ -47,8 +47,129 @@ describe("ErrorStore", function() {
           expect(data.size).to.be(1);
           expect(data.get(0).get("message1")).to.be("message one");
           expect(data.get(0).get("message2")).to.be("message two");
+          expect(data.get(0).get("id")).to.be.ok();
         },
       ]
     ).then(() => { done() });
   });
+  
+  it("should hold two errors", function(done) {
+    helper.observe(
+      ({ errorNotificationAction }) => {
+        errorNotificationAction.onNext(Immutable.Map({
+          message1: "message 1-1",
+          message2: "message 1-2",
+        }));
+        errorNotificationAction.onNext(Immutable.Map({
+          message1: "message 2-1",
+          message2: "message 2-2",
+        }));
+      },
+      [
+        data => {
+          expect(data.size).to.be(0);
+        },
+        data => {
+          expect(data.size).to.be(1);
+          expect(data.get(0).get("message1")).to.be("message 1-1");
+          expect(data.get(0).get("message2")).to.be("message 1-2");
+        },
+        data => {
+          expect(data.size).to.be(2);
+          expect(data.get(0).get("message1")).to.be("message 1-1");
+          expect(data.get(0).get("message2")).to.be("message 1-2");
+          expect(data.get(1).get("message1")).to.be("message 2-1");
+          expect(data.get(1).get("message2")).to.be("message 2-2");
+        },
+      ]
+    ).then(() => { done() });
+  });
+  
+  it("should clear the error", function(done) {
+    helper.observe(
+      ({ errorNotificationAction }) => {
+        errorNotificationAction.onNext(Immutable.Map({
+          message1: "message 1-1",
+          message2: "message 1-2",
+        }));
+        errorNotificationAction.onNext(Immutable.Map({
+          message1: "message 2-1",
+          message2: "message 2-2",
+        }));
+      },
+      [
+        data => {
+          expect(data.size).to.be(0);
+        },
+        data => {
+          expect(data.size).to.be(1);
+        },
+        data => {
+          expect(data.size).to.be(2);
+          expect(data.get(0).get("message1")).to.be("message 1-1");
+          expect(data.get(0).get("message2")).to.be("message 1-2");
+          expect(data.get(1).get("message1")).to.be("message 2-1");
+          expect(data.get(1).get("message2")).to.be("message 2-2");
+        },
+      ]
+    ).then((data) => {
+      const id = data.get(0).get("id");
+      return helper.observe(
+        ({ clearErrorAction }) => {
+          clearErrorAction.onNext(Immutable.Map({
+            id: id,
+          }));
+        },
+        [
+          data => {
+            expect(data.size).to.be(1);
+            expect(data.get(0).get("message1")).to.be("message 2-1");
+            expect(data.get(0).get("message2")).to.be("message 2-2");
+          },
+        ]
+      );
+    }).then(() => { done() });    
+  });
+  
+  it("should clear all errors", function(done) {
+    helper.observe(
+      ({ errorNotificationAction }) => {
+        errorNotificationAction.onNext(Immutable.Map({
+          message1: "message 1-1",
+          message2: "message 1-2",
+        }));
+        errorNotificationAction.onNext(Immutable.Map({
+          message1: "message 2-1",
+          message2: "message 2-2",
+        }));
+      },
+      [
+        data => {
+          expect(data.size).to.be(0);
+        },
+        data => {
+          expect(data.size).to.be(1);
+        },
+        data => {
+          expect(data.size).to.be(2);
+          expect(data.get(0).get("message1")).to.be("message 1-1");
+          expect(data.get(0).get("message2")).to.be("message 1-2");
+          expect(data.get(1).get("message1")).to.be("message 2-1");
+          expect(data.get(1).get("message2")).to.be("message 2-2");
+        },
+      ]
+    ).then((data) => {
+      const id = data.get(0).get("id");
+      return helper.observe(
+        ({ clearAllErrorsAction }) => {
+          clearAllErrorsAction.onNext();
+        },
+        [
+          data => {
+            expect(data.size).to.be(0);
+          },
+        ]
+      );
+    }).then(() => { done() });    
+  });  
 });
